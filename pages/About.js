b8e36b708a27bfd08d8dd0e7fe55b728cae9aa3c
OmarSaber-os <osaber218@gmail.com>
2021-06-16 00:20:28 +0200

    :rocket:first commit

---
+++ pages/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+      lorem epsum lorem epsumlorem epsumlorem epsumlorem epsumlorem epsumlorem
+      epsumlorem epsumlorem epsumlorem epsumlorem epsumlorem epsumlorem
+      epsumlorem epsumlorem epsumlorem epsumlorem epsumlorem epsumlorem epsum
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/pockmon/index.js
@@ -0,0 +1,5 @@
+const Pockmon = () => {
+  return <h1>Pockmon by nextjs</h1>;
+};
+
+export default Pockmon;
